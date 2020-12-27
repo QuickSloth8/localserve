988e90229f24995ddec8985a9f30f193114a3e14
QuickSloth8 <quicksloth8@gmail.com>
2020-12-27 15:33:43 +0200

    running on default addr, with a dummy default handle

--- localserve/cmd/serve.go
+++ localserve/cmd/serve.go
@@ -3,9 +3,16 @@ package cmd
 import (
   "fmt"
   "github.com/spf13/cobra"
+  "github.com/spf13/viper"
+  "net/http"
+  "log"
 )
 
 func init() {
+  // default listening addr and port
+  viper.SetDefault("listenAddr", "127.0.0.1")
+  viper.SetDefault("listenPort", "3223")
+
   rootCmd.AddCommand(serveCmd)
 }
 
@@ -14,6 +21,24 @@ var serveCmd = &cobra.Command{
   Short: "Start server",
   Long:  `Start server on your device`,
   Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("Starting Server ...")
+    log.Fatal(startServer())
   },
+}
+
+func getListenAddr() string {
+  listenAddr := viper.GetString("listenAddr")
+  listenPort := viper.GetString("listenPort")
+  return listenAddr + ":" + listenPort
+}
+
+func startServer() error {
+  http.HandleFunc("/", indexHandler)
+
+  fmt.Println("Starting server at", getListenAddr())
+
+  return http.ListenAndServe(getListenAddr(), nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte("indexHandler was called\n"))
 }
