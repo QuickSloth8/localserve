b4e068097b508304a61c95d484bd95e55e413cf5
QuickSloth8 <quicksloth8@gmail.com>
2020-12-28 08:30:16 +0200

    port & ip command flags

--- localserve/cmd/serve.go
+++ localserve/cmd/serve.go
@@ -6,23 +6,38 @@ import (
   "github.com/spf13/viper"
   "net/http"
   "log"
+  "os"
+)
+
+
+
+var (
+  listenAddr string
+  listenPort string
+
+  serveCmd = &cobra.Command{
+    Use:   "serve",
+    Short: "Start server",
+    Long:  `Start server on your device`,
+    Run: func(cmd *cobra.Command, args []string) {
+      log.Fatal(startServer())
+    },
+  }
 )
 
 func init() {
-  // default listening addr and port
-  viper.SetDefault("listenAddr", "127.0.0.1")
-  viper.SetDefault("listenPort", "3223")
+  serveCmd.PersistentFlags().StringVar(&listenAddr, "listenAddr", "127.0.0.1", "The IP to listen on")
+  serveCmd.PersistentFlags().StringVar(&listenPort, "listenPort", "3223", "The port to listen on")
+  viper.BindPFlag("listenAddr", serveCmd.PersistentFlags().Lookup("listenAddr"))
+  viper.BindPFlag("listenPort", serveCmd.PersistentFlags().Lookup("listenPort"))
+
+  serveRoot, err := os.Getwd()
+  if err != nil {
+    panic(err)
+  }
+  viper.SetDefault("serveRoot", serveRoot)
 
   rootCmd.AddCommand(serveCmd)
-}
-
-var serveCmd = &cobra.Command{
-  Use:   "serve",
-  Short: "Start server",
-  Long:  `Start server on your device`,
-  Run: func(cmd *cobra.Command, args []string) {
-    log.Fatal(startServer())
-  },
 }
 
 func getListenAddr() string {
@@ -32,10 +47,10 @@ func getListenAddr() string {
 }
 
 func startServer() error {
-  rootDir := "./test-dir"
-  fs := http.FileServer(http.Dir(rootDir))
+  serveRoot := viper.GetString("serveRoot")
+  fs := http.FileServer(http.Dir(serveRoot))
 
   fmt.Println("Starting server at", getListenAddr())
-  fmt.Println("Serving files in directory", rootDir)
+  fmt.Println("Serving files in directory", serveRoot)
   return http.ListenAndServe(getListenAddr(), fs)
 }
